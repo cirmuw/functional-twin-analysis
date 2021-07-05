0b713c37d0168ec34d8003bfd9ce838ee9ebdc77
bburger13 <85995306+bburger13@users.noreply.github.com>
2021-07-05 12:02:33 +0200

    Update settings.py

--- Code/settings.py
+++ Code/settings.py
@@ -1,3 +1,3 @@
-projectfolder='/home/cir/bburger/PHD/Github/Heritability/' # project folder, root directory of output
-HCProot='/home/cir/bburger/Datasets/head/cir_georg_2018_hcp1000/extractedData/'              #folder where HCP data is stored
-HCP_information_sheet_path='/home/cir/bburger/PHD/HCP_SubjectData/RESTRICTED_georglangs_11_24_2015_16_5_58.xlsx' # file containing zygosity information of subjects
+projectfolder='/path/to/project/folder/' # project folder, root directory of output
+HCProot='/root/folder/of/HCP/data/'              #folder where HCP data is stored
+HCP_information_sheet_path='/path/to/HCP_subject_info_restricted.xlsx' # file containing zygosity information of subjects
